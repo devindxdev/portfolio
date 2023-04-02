6126c090918795ff5810b758d5ecacfdc7ebae63
Devind Dev <devindxdev@gmail.com>
2023-04-02 02:55:35 -0700

    a lotta changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
 'use strict';
 
-// element toggle function
 const elementToggleFunc = function (elem) { 
     elem.classList.toggle("active"); 
-    console.log(elem.classList);
 }
 
-// sidebar variables
-const sidebar = document.querySelector("[profileBarData]");
-const sidebarBtn = document.querySelector("[profileBarButton]");
+const profileBar = document.querySelector("[profileBarData]");
+const profileBarButton = document.querySelector("[profileBarButton]");
 
-// sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+profileBarButton.addEventListener("click", function () { elementToggleFunc(profileBar); });
+
+//Navigation
+const navigationLinks = document.querySelectorAll("[navLinkData]");
+const pages = document.querySelectorAll("[data-page]");
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+    for (let i = 0; i < pages.length; i++) {
+        console.log(pages[i].dataset);
+        if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+  });
+
+}
